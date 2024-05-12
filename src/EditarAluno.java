0808d513402b253a382af8ffa59a0c713be1a4a4
Jonas Avelino <jonasavma@gmail.com>
2024-05-12 00:16:51 -0300

    aula 44 Instanciando um objeto de configuração no hibernate

--- src/EditarAluno.java
+++ src/EditarAluno.java
@@ -1,41 +1,34 @@
 
-
-
-
 import javax.swing.JOptionPane;
 import org.hibernate.*;
 import org.hibernate.cfg.*;
 
-
-
-
-
 public class EditarAluno {
-	public static void main(String[] args) {
 
-		try {
-              SessionFactory fabrica = new Configuration().configure().buildSessionFactory();
-              Session  sessao = fabrica.openSession();
-              Aluno aluno = new Aluno();
-              aluno.setAlu_codigo(2);
-              aluno.setAlu_nome("Jonas avelino");
-              aluno.setAlu_curso("hibernate");
-              aluno.setAlu_fone("969403632");
-              aluno.setAlu_cidade("Nova Iguaçu");
-              
-              Transaction tx_aluno= sessao.beginTransaction();
-              
-              sessao.saveOrUpdate(aluno);
-              
-              tx_aluno.commit();
-              sessao.close();
-			
-			
-		} catch (Exception e) {
-               JOptionPane.showMessageDialog(null, "Erro de insersção :"+ e);
-		
-		}
+    public static void main(String[] args) {
 
-	}
+        try {
+            SessionFactory fabrica = new Configuration().configure().buildSessionFactory();
+            Session sessao = fabrica.openSession();
+            Aluno aluno = new Aluno();
+            aluno.setAlu_codigo(2);
+            aluno.setAlu_nome("Jonas avelino");
+            aluno.setAlu_curso("hibernate");
+            aluno.setAlu_fone("969403632");
+            aluno.setAlu_cidade("Nova Iguaçu");
+
+            Transaction tx_aluno = sessao.beginTransaction();
+
+            sessao.saveOrUpdate(aluno);
+
+            tx_aluno.commit();
+            sessao.close();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro de insersção :" + e);
+
+        }
+
+    }
 
 }
---
+++ src/ListarAlunos.java
@@ -0,0 +1,48 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class ListarAlunos {
+
+    public static void main(String[] args) {
+
+        try {
+
+            SessionFactory fabrica = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
+                    .setProperty("hibernate.connection.url","jdbc:postgresql://localhost:5433/javahibernate")
+                    .setProperty("hibernate.connection.username", "postgres")
+                    .setProperty("hibernate.connection.password", "root")
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
+                    .setProperty("current_session_context_class", "threa")
+                    .setProperty("hbm2ddl.auto", "update")
+                    .setProperty("pool_size", "10")
+                    .configure().buildSessionFactory();
+            Session sessao = fabrica.openSession();
+
+            List<Aluno> lista_aluno = new ArrayList();
+            lista_aluno = sessao.createQuery("from Aluno").list();
+            int tamanho_lista = lista_aluno.size();
+            String dados = "";
+            for (int i = 0; i < tamanho_lista; i++) {
+                Aluno aluno = lista_aluno.get(i);
+                System.out.println("Codigo :" + aluno.getAlu_codigo() + "Nome :" + aluno.getAlu_nome() + "Curso :" + aluno.getAlu_fone() + "Fone :" + "Cidade :" + aluno.getAlu_cidade());
+                dados = dados + "\n" + "Codigo : " + aluno.getAlu_codigo() + " Nome : " + aluno.getAlu_nome() + " Curso : " + aluno.getAlu_curso() + " Fone : " + aluno.getAlu_fone() + " Cidade : " + aluno.getAlu_cidade();
+
+            }
+            JOptionPane.showMessageDialog(null, dados);
+            sessao.close();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar alunos :" + e);
+
+        }
+
+    }
+
+}
