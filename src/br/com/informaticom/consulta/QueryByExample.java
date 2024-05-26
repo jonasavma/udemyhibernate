c4bbbaeb6085ee36f0374d0091aad20a9e1ac47b
Jonas Avelino <jonasavma@gmail.com>
2024-05-25 22:56:34 -0300

    aula 101 Expression Criteria para filtrar no hibernate & aula 103 Criteria com QueryByExample QBE no Hibernate

--- src/br/com/informaticom/consulta/QueryByExample.java
+++ src/br/com/informaticom/consulta/QueryByExample.java
@@ -6,37 +6,28 @@ import java.util.List;
 import javax.swing.JOptionPane;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.criterion.Expression;
+import org.hibernate.criterion.Example;
 
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
-import org.hibernate.hql.ast.tree.RestrictableStatement;
 
-public class API_criteria {
+public class QueryByExample {
 
     public static void main(String[] args) {
         try {
 
             Session sessao = HibernateUtil.getSessao();
-            Criteria listagem_criteria = sessao.createCriteria(Aluno.class);
-            listagem_criteria.setMaxResults(10);
-            listagem_criteria.setFirstResult(0);
-            listagem_criteria.addOrder(Order.asc("alu_nome"));
-            //Forma de pesquisa tres ao todo.
-            //forma 1
-            // listagem_criteria.add(Expression.eq("alu_nome", "Jonas Martins"));
-            listagem_criteria.add(Restrictions.between("alu_codigo", 1, 9));
-            //Forma 2
-            // String[] cidades = {"Nova Iguaçu", "Niteroi"};
-            // listagem_criteria.add(Restrictions.in("alu_cidade", cidades));
-            //Forma 3
-            listagem_criteria.add(Restrictions.like("alu_cidade", "Niteroi"));
-            List<Aluno> lista_alunos = listagem_criteria.list();
+            Aluno aluno = new Aluno();
+            aluno.setAlu_cidade("Nova Iguaçu");
+            Criteria criteria = sessao.createCriteria(Aluno.class);
+            criteria.add(Example.create(aluno));
+
+            List<Aluno> lista_alunos = criteria.list();
             int tamanho_lista = lista_alunos.size();
             String dados = "";
 
             for (int i = 0; i < tamanho_lista; i++) {
-                Aluno aluno = lista_alunos.get(i);
+                aluno = lista_alunos.get(i);
                 System.out.println("Codigo :" + aluno.getAlu_codigo() + "-" + "Nome :" + aluno.getAlu_nome() + "|" + "Curso : " + aluno.getAlu_fone() + "Fone :" + "Cidade :" + aluno.getAlu_cidade());
                 dados = dados + "\n" + "Codigo : " + aluno.getAlu_codigo() + "-" + " Nome : " + aluno.getAlu_nome() + "|" + " Curso : " + aluno.getAlu_curso() + " Fone : " + aluno.getAlu_fone() + " Cidade : " + aluno.getAlu_cidade();
 
