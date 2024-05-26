9b1ec0444abb7639e9cdff89079f820771d42862
Jonas Avelino <jonasavma@gmail.com>
2024-05-25 21:30:05 -0300

    aula 98 Interface Query sql nativo createSQLQuery

---
+++ src/br/com/informaticom/consulta/HQL_query_between.java
@@ -0,0 +1,43 @@
+package br.com.informaticom.consulta;
+
+import br.com.informaticom.modelo.Aluno;
+import br.com.informaticom.util.HibernateUtil;
+import java.util.List;
+import javax.swing.JOptionPane;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+public class HQL_query_between {
+
+    public static void main(String[] args) {
+        try {
+
+            Session sessao = HibernateUtil.getSessao();
+            //Query listagem_query = sessao.createQuery("from Aluno where alu_codigo between 1  and 15");
+            // listagem_query.setString("nome", "Jonas Martins");
+            //ou 
+            Query listagem_query = sessao.createQuery("from Aluno where alu_codigo between :codini and :codfinal ");
+            listagem_query.setInteger("codini", 1);
+            listagem_query.setInteger("codfinal", 10);
+
+            List<Aluno> lista_alunos = listagem_query.list();
+            int tamanho_lista = lista_alunos.size();
+            String dados = "";
+
+            for (int i = 0; i < tamanho_lista; i++) {
+                Aluno aluno = lista_alunos.get(i);
+                System.out.println("Codigo :" + aluno.getAlu_codigo() + "Nome :" + aluno.getAlu_nome() + "Curso :" + aluno.getAlu_fone() + "Fone :" + "Cidade :" + aluno.getAlu_cidade());
+                dados = dados + "\n" + "Codigo : " + aluno.getAlu_codigo() + " Nome : " + aluno.getAlu_nome() + " Curso : " + aluno.getAlu_curso() + " Fone : " + aluno.getAlu_fone() + " Cidade : " + aluno.getAlu_cidade();
+
+            }
+            JOptionPane.showMessageDialog(null, dados);
+            sessao.close();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar alunos :" + e);
+
+        }
+
+    }
+
+}
---
+++ src/br/com/informaticom/consulta/Query_sql_nativo.java
@@ -0,0 +1,40 @@
+package br.com.informaticom.consulta;
+
+import br.com.informaticom.modelo.Aluno;
+import br.com.informaticom.util.HibernateUtil;
+import java.util.List;
+import javax.swing.JOptionPane;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author jonas
+ */
+public class Query_sql_nativo {
+
+    public static void main(String[] args) {
+        try {
+
+            Session sessao = HibernateUtil.getSessao();
+            Query listagem_query = sessao.createSQLQuery("select *from alunos").addEntity(Aluno.class);
+            List<Aluno> lista_alunos = listagem_query.list();
+            int tamanho_lista = lista_alunos.size();
+            String dados = "";
+
+            for (int i = 0; i < tamanho_lista; i++) {
+                Aluno aluno = lista_alunos.get(i);
+                System.out.println("Codigo :" + aluno.getAlu_codigo() + "Nome :" + aluno.getAlu_nome() + "Curso :" + aluno.getAlu_fone() + "Fone :" + "Cidade :" + aluno.getAlu_cidade());
+                dados = dados + "\n" + "Codigo : " + aluno.getAlu_codigo() + " Nome : " + aluno.getAlu_nome() + " Curso : " + aluno.getAlu_curso() + " Fone : " + aluno.getAlu_fone() + " Cidade : " + aluno.getAlu_cidade();
+
+            }
+            JOptionPane.showMessageDialog(null, dados);
+            sessao.close();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar alunos :" + e);
+
+        }
+
+    }
+}
